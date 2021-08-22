da232a6202a9bc2767b1915ea74a5c128b26fbaa
Adham <43854216+0xb77a9@users.noreply.github.com>
2021-08-22 20:54:50 +0200

    Add files via upload

---
+++ MovingText.lua
@@ -0,0 +1,20 @@
+-- Moving Text
+-- Made By BETA
+
+-- Locals
+local x, y = client.screen_size()
+local loc = x
+y = 0
+
+-- GUI
+local Enable = gui.new_checkbox("Enable", false)
+
+-- Functions
+function main(text)
+    if (loc == -20) then loc = x + 20 end
+    loc = loc - 1
+    render.text(text, loc, 0, 15, 255, 255, 255, 255)
+end
+
+-- Callbacks
+callbacks.register("paint", main)
